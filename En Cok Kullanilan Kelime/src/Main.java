0b3a7907f6c2c87f7b68c11a77671fd1b8189523
Oğuzhan Furkan Özdemir <ofozdemir14@gmail.com>
2024-01-11 01:59:28 +0300

    En Çok Kelime

---
+++ En Cok Kullanilan Kelime/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Bir metin girin: ");
+        String words = scan.nextLine();
+        String[] word = words.split("\\s+");
+
+        Map<String, Integer> wordCounts = new HashMap<>();
+
+
+        for (String count : word) {
+            if (wordCounts.containsKey(count)) {
+                wordCounts.put(count, wordCounts.get(count) + 1);
+            } else {
+                wordCounts.put(count, 1);
+            }
+        }
+
+        int maxCount = 0;
+        String mostFrequentWord = "";
+
+        for (Map.Entry<String, Integer> entry : wordCounts.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                maxCount = entry.getValue();
+                mostFrequentWord = entry.getKey();
+            }
+        }
+
+        System.out.println("En sık geçen kelime: " + mostFrequentWord + " (" + maxCount + " kez)");
+    }
+}
